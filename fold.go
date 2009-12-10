c5284a481be49adf3809e1d508af5ad72c3eb726
John Asmuth <jasmuth@gmail.com>
2009-12-10 00:11:35 -0500

    modified Fold() to not do any busywaiting

--- fold.go
+++ fold.go
@@ -1,81 +1,61 @@
 package conc
 
-import (
-	"sync";
-)
-
-type safeCounter struct {
-	count int;
-	m sync.Mutex;
-}
-func (s *safeCounter) incr() {
-	s.m.Lock();
-	s.count++;
-	s.m.Unlock();
-}
-func (s *safeCounter) decr() {
-	s.m.Lock();
-	s.count--;
-	s.m.Unlock();
-}
-func (s *safeCounter) val() int {
-	return s.count;
-}
-
-//try to read a value off a channel until foo returns false
-func grabUntil(in chan Box, foo func() bool) (v Box, ok bool) {
-	for {
-		if v, ok = <- in; ok {
-			return;
-		}
-		if !foo() {
-			break;
-		}
-	}
-	return;
-}
-
 /*
 	concurrent folding - foo is applied to pairs of items and the results are put
-	back in the list, until there is only one item in the list.
+	back in the list, until there is only one item in the list. The operator provided
+	must be both associative and commutative.
 */
-
-func Fold(foo func(Box, Box) Box, in chan Box) Box {
+func Fold(foo func(Box, Box) Box, in chan Box) (result Box) {
 	ready := make(chan Box);
+	
+	inserted := make(chan bool);
+	count := 0;
+	
+	doneSignal := make(chan bool);
 	
 	//dump all values from in into ready
 	go func() {
-		for i := range in {
-			ready <- i;
+		for {
+			if v, isLast := <-in, closed(in); !isLast {
+				count++;
+				go func() {
+					ready <- v;
+					inserted <- true;
+				}();
+			}
+			else {
+				break;
+			}
 		}
+		for i:=0; i<count; i++ {
+			<-inserted;
+		}
+		doneSignal <- true;
 	}();
 	
-	//keep track of how many values are not in the ready queue, but will be eventually
-	inProcess := new(safeCounter);
-	
-	//if this function returns false, all relevant values are currently in the ready queue
-	moreComing := func() bool {
-		return !closed(in) || inProcess.val() > 0;
-	};
-	
+	doneReading := false;
+	folds := 0;
 	for {
 		first := <- ready;
-		
-		second, ok := grabUntil(ready, moreComing);
-		
-		//if there aren't any more values coming, then we have completed the folding process
-		if !ok {
-			return first;
+		if doneReading && folds == count-1 {
+			result = first;
+			break;
 		}
-		
-		//indicate that there is a value not yet in the ready queue that needs to be processed
-		inProcess.incr();
-		go func() {
-			v := foo(first, second);
-			ready <- v;
-			inProcess.decr();
-		}();
+		select {
+		case second := <- ready:
+			folds++;
+			go func() {
+				ready <- foo(first, second);
+			}();
+			break;
+		case <-doneSignal:
+			doneReading = true;
+			go func() {
+				ready <- first;
+			}();
+			break;
+		}
 	}
 	
-	return <-ready;
+	return;
 }
